0daf3922d5b03d978528fc72ab24d0b5957efd57
waitlee <me@leephp.com>
2013-07-05 18:04:53 +0800

    add index file

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 
-echo "LACFRAMEWORK is working!";
-echo "git commit!";
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', realpath(dirname(__FILE__)));
+define('F_PATH', ROOT_PATH . DS . 'lacframework');
+
+require(F_PATH . DS ."lac.php");
+
+$l = new LAC();
+$l->run();
+
